1b7cfe1071857126ef967a9b688dbd00b2300813
SpartanB-312 <zhanghw312@foxmail.com>
2023-06-27 15:50:22 +0800

    单摆微调

--- Pendulum/src/Fcn4ImplicitEuler.m
+++ Pendulum/src/Fcn4ImplicitEuler.m
@@ -3,11 +3,11 @@ function FcnOut=Fcn4ImplicitEuler(xIn)
 global xi;
 h=0.001;
 alpha = 25; beta = 50; %经验值5~50
+%alpha = 0; beta = 0; %
 q=xIn(1:3);v=xIn(4:6);
 %
-A = [1 0 0; 0 1 0; 0 0 1];
-B = [0; -9.8;-9.8 * sin(pi / 6)];
-B(3, 1) = -9.8 * sin(q(3));
+A = [1 0 0; 0 1 0; 0 0 0];
+B = [0; -9.8;0];
 P(1,1) = -v(3) ^ 2 * cos(q(3)); P(2,1) = v(3) ^ 2 * sin(q(3)); %加速度约束
 phi = [q(1) - sin(q(3)); q(2) + cos(q(3))];
 phiq = [1 0 -cos(q(3)); 0 1 -sin(q(3))];
@@ -20,5 +20,5 @@ a=X(1:3);
 delta=[v;a];
 
 FcnOut=[xi+h*delta-[q;v];
-        phiq * v];
+       alpha*phiq * v+beta*phi];
 end
