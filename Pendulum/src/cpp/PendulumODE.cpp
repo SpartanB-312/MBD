2545701e9dbec67c0de14996c590c5791cafb4d6
SpartanB-312 <zhanghw312@foxmail.com>
2023-07-05 22:05:44 +0800

    cpp

--- Pendulum/src/cpp/PendulumODE.cpp
+++ Pendulum/src/cpp/PendulumODE.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <Eigen/Dense>
 #include <math.h>
 
 #define _USE_MATH_DEFINES // 使用math.h中的M_PI宏定义需要
 
 using Eigen::MatrixXd;
- 
+using namespace std;
+
 int main()
 {
   float g=-9.8;
@@ -17,15 +19,18 @@ int main()
   MatrixXd v(1,n);
   MatrixXd a(1,n);
 
-  q(1,1)=pi;
-  v(1,1)=0;
-  a(1,1)=g*sin(q(1,1));
+  q(0,0)=pi/3;
+  v(0,0)=0;
+  a(0,0)=g*sin(q(1,1));
 
-  for(int i=2;i<=n;i++)
+  for(int i=1;i<n;i++)
   {
     q(1,i)=q(1,i-1)+v(1,i-1)*h;
     v(1,i)=v(1,i-1)+a(1,i-1)*h;
     a(1,i)=g*sin(q(1,i));
   }
-  std::cout << q << std::endl;
+  //cout << q(1,1) << endl;
+  ofstream fout("test.txt");
+  fout << q;
+  fout.close();
 }
