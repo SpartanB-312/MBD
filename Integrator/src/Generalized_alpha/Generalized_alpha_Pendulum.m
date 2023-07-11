92db2ac7c937cd1a8b1cc4249868fe0f303e429c
SpartanB-312 <zhanghw312@foxmail.com>
2023-07-11 11:33:22 +0800

    广义alpha未完成

--- Integrator/src/Generalized_alpha/Generalized_alpha_Pendulum.m
+++ Integrator/src/Generalized_alpha/Generalized_alpha_Pendulum.m
@@ -4,62 +4,85 @@ B = [0; -9.8; 0];%-9.8 * sin(pi / 6)];
 q0 = [0.5; -sqrt(3) / 2; pi / 6]; v0 = [0; 0; 0]; %初始状态
 phi = [q0(1) - sin(pi / 6); q0(2) + cos(pi / 6)]; %约束
 phiq = [1 0 -cos(pi / 6); 0 1 -sin(pi / 6)]; %雅可比
-h = 0.001; T = 5; t = 0:h:T; %步长及时间
+h = 0.01; T = 5; t = 0:h:T; %步长及时间
 
 column = zeros(3, (length(t) - 2));
 q = q0;
 v = v0;
 a = zeros(3, length(t));
 E = eye(3);
-alpha = 25; beta = 50; %经验值5~50
+Alpha = 25; Beta = 50; %经验值5~50
 %alpha=1/h;beta=sqrt(2)/h;
 P = zeros(2, 1);
 P(1) = -v(3) ^ 2 * cos(q(3)); P(2) = v(3) ^ 2 * sin(q(3)); %加速度约束
 phiT = phiq * v; %对时间全微分
-P1 = P - 2 * alpha * phiT - beta ^ 2 * phi; %稳定形式
+P1 = P - 2 * Alpha * phiT - Beta ^ 2 * phi; %稳定形式
 LEFT = [A phiq'; phiq zeros(2)]; %左端系数
 RIGHT = [B; P1]; %右端
 X = (LEFT ^ -1) * RIGHT; %上三行加速度；下两行lamda
 a(1, 1) = X(1); a(2, 1) = X(2); a(3, 1) = X(3);
+l(:,1)=X(4:5);
 v(:, 2) = v(:, 1) + h* a(:, 1);
 q(:, 2) = q(:, 1) + h * v(:, 1);
 %% 组装
-q = [q column]; v = [v column];
+q = [q column]; v = [v column]; l=[l column(1:2,:)];
 %% 广义alpha
-%后缀加i与Baumgrate区分
-alphai=0;%α在[0,1/3]
-gammai=0+alphai;betai=0.25*(0.5+gammai)^2;
+alpha=0;%α在[0,1/3]
+gamma=0+alpha;beta=0.25*(0.5+gamma)^2;
+p=0.5;
+alpham=(2*p-1)/(1+p);alphaf=p/(1+p);
+betai=(1-alpham)/(h^2*beta*(1-alphaf));
+gammai=gamma/(h*beta);
 
-for i = 2:(length(t) - 1)
-    %B(3, 1) = -9.8 * sin(q(3, i));
-    P(1) = -v(3, i) ^ 2 * cos(q(3, i)); P(2) = v(3, i) ^ 2 * sin(q(3, i)); %加速度约束
-    phi = [q(1, i) - sin(q(3, i)); q(2, i) + cos(q(3, i))];
-    phiq = [1 0 -cos(q(3, i)); 0 1 -sin(q(3, i))];
-    phiT = phiq * v(:, i);
-    P1 = P - 2 * alpha * phiT - (beta ^ 2) * phi;
-    LEFT = [A phiq'; phiq zeros(2)];
-    RIGHT = [B; P1];
-    X = (LEFT ^ -1) * RIGHT;
-    a(1, i) = X(1); a(2, i) = X(2); a(3, i) = X(3);
-    v(:, i + 1) = v(:, i) + h*(1-gammai) * a(:, i)+h*gammai*a(:,i-1);
-    q(:, i + 1) = q(:, i) + h * v(:, i)+(0.5-betai)*(h^2)*a(:,i-1)+betai*(h^2)*a(:,i-1);
-    at=a(:,i);
-    vt=v(:,i)+h*(1-gammai)*a(:,i)+h*gammai*at;
-    qt=q(:,i)+h*v(:,i)+(0.5-betai)*h^2*a(:,i-1)+betai*h^2*at;
-    x=0;
-    for j=1:10
+at=a(:,1);
 
-        P(1) = -vt(3) ^ 2 * cos(qt(3)); P(2) = vt(3) ^ 2 * sin(qt(3)); %加速度约束
-        phi = [qt(1) - sin(qt(3)); qt(2) + cos(qt(3))];
-        phiq = [1 0 -cos(qt(3)); 0 1 -sin(qt(3))];
-        phiT = phiq * vt;
-        P1 = P - 2 * alpha * phiT - (beta ^ 2) * phi;
-        LEFT = [A phiq'; phiq zeros(2)];
-        RIGHT = [B; P1];
-        X = (LEFT ^ -1) * RIGHT;
-        x=X(1:3)-at;
-        vt=vt+h*gammai*x;
-        qt=qt+betai*h^2*x;
+for i = 1:(length(t) - 1)
+
+    q(:,i+1)=q(:,i)+h*v(:,i)+0.5*h^2*(1-2*beta)*at;
+    v(:,i+1)=v(:,i)+h*(1-gamma)*at;
+    l(:,i+1)=0;
+    at=1/(1-alpham)*(alphaf*a(:,i)-alpham*at);
+    q(:,i+1)=q(:,i+1)+h^2*beta*at;
+    v(:,i+1)=v(:,i+1)+h*gamma*at;
+    a(:,i+1)=0;
+    detw=ones(1,5);
+    j=1;
+
+    while max(detw)>1e-3
+        q(:,i+1)=q(:,i)+h*v(:,i)+0.5*h^2*((1-2*beta)*a(:,i)+2*beta*a(:,i));
+        v(:,i+1)=v(:,i)+h*((1-gamma)*a(:,i)+gamma*a(:,i+1));
+
+        Kt=diag([0,0,l(1,i+1)*sin(q(3,i+1))+l(2,i+1)*sin(q(3, i+1))]);
+        Ct=0;
+        
+        P(1) = -v(3, i+1) ^ 2 * cos(q(3, i+1)); P(2) = v(3, i+1) ^ 2 * sin(q(3, i+1)); %加速度约束
+        phi = [q(1, i+1) - sin(q(3, i+1)); q(2, i+1) + cos(q(3, i+1))];
+        phiq = [1 0 -cos(q(3, i+1)); 0 1 -sin(q(3, i+1))];
+        phiT = phiq * v(:, i+1);
+        P1 = P - 2 * Alpha * phiT - (Beta ^ 2) * phi;
+        St = [A*betai+Ct*gammai+Kt phiq'; phiq zeros(2)];
+        rl=phi;
+        rq=B-A*q(:,i+1)-phiq'*l(:,i+1);
+        RIGHT = [rq; rl];
+        detw = St\RIGHT;
+        detq=detw(1:3);
+        detl=detw(4:5);
+
+        q(:,i+1)=q(:,i+1)+detq;
+        v(:,i+1)=v(:,i+1)+gammai*detq;
+        a(:,i+1)=a(:,i+1)+betai*detq;
+        l(:,i+1)=l(:,i+1)+detl;
+
+        %a(:,i+1)=a(:,i+1)-detw(1:3);
+        %l(:,i+1)=l(:,i+1)-detw(4:5);
+
+
+        j=j+1;
+        if j>100
+            break;
+        end
     end
-    a(:,i)=at+x;
+
+    at=at+(1-alphaf)/(1-alpham)*a(:,i+1);
+
 end
